1d0148c8dfd20747a18ea3ca9f2e0dbaa8a65d9d
racsor <racsor@gmail.com>
2014-02-02 19:01:15 +0100

    CLEAN

--- src/main/java/org/alterq/mvc/QuinielaController.java
+++ src/main/java/org/alterq/mvc/QuinielaController.java
@@ -1,9 +1,7 @@
 package org.alterq.mvc;
 
-import org.alterq.repo.MemberDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/quiniela")
 public class QuinielaController {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	@Autowired
-	private MemberDao memberDao;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String initPage(Model model) {
