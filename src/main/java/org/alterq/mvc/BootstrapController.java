cc96c8d0394ea8a521265532f413d7a812593b9f
scabellog <scabellog@gmail.com>
2014-02-05 00:34:02 +0100

    WEB - Bootstrap Template

---
+++ src/main/java/org/alterq/mvc/BootstrapController.java
@@ -0,0 +1,43 @@
+package org.alterq.mvc;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.alterq.dto.ResponseDto;
+import org.alterq.repo.SessionAlterQDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = { "/", "/bootstrap" })
+public class BootstrapController {
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private SessionAlterQDao sessionDao;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String initPage() {
+		log.debug("init bootstrap.jsp");
+		return "bootstrap";
+	}
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseDto logout(@CookieValue(value = "session", defaultValue = "") String cookieSession, HttpServletResponse response) {
+		log.debug("init LoginController.logout");
+		log.debug("session:" + cookieSession);
+		sessionDao.endSession(cookieSession);
+		Cookie sessionCookie=new Cookie("session","");
+		sessionCookie.setMaxAge(0);
+		sessionCookie.setPath("/quinimobile");
+		response.addCookie(sessionCookie);
+		ResponseDto dto = new ResponseDto();
+		return dto;
+	}
+
+}
