a80696dd15b7ef167d9aed0729d141d3b10ceae9
scabellog <scabellog@gmail.com>
2014-03-11 23:31:31 +0100

    MVC - Admin - Open Round

--- src/main/java/org/alterq/mvc/AdminController.java
+++ src/main/java/org/alterq/mvc/AdminController.java
@@ -1,13 +1,20 @@
 package org.alterq.mvc;
 
 
+import java.util.List;
+
+import org.alterq.domain.GeneralData;
+import org.alterq.repo.GeneralDataDao;
 import org.alterq.repo.SessionAlterQDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = { "/admin" })
@@ -15,10 +22,29 @@ public class AdminController {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private SessionAlterQDao sessionDao;
+	private GeneralDataDao dao;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String initPage() {
 		log.debug("init admin.jsp");
 		return "admin";
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, produces = "application/json", value = "{company, round, season}")
+	public @ResponseBody 
+	void openRound(@PathVariable int company, @PathVariable int round, @PathVariable int season) {
+		GeneralData generalData = new GeneralData();
+		generalData.setActive(true);
+		generalData.setCompany(company);
+		generalData.setRound(round);
+		generalData.setSeason(season);
+		
+		log.debug("openRound");
+		//if exist, update active=true
+		dao.update(generalData);
+		//return the actual round/session
+		return;
+	}
+
+	
 }
---
+++ src/test/java/org/alterq/mvc/test/AdminControllerTest.java
@@ -0,0 +1,53 @@
+package org.alterq.mvc.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.alterq.domain.GeneralData;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:/spring/applicationContext.xml", "classpath:/spring/app-servlet.xml" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AdminControllerTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void AA_openRound() throws Exception {
+		ResultActions ra = this.mockMvc.perform(post("/admin/season/2013/round/11/open"));
+		ra.andDo(MockMvcResultHandlers.print());
+		ra.andExpect(status().isOk());
+		
+//		bean = mapper.readValue(ra.andReturn().getResponse().getContentAsString(), GeneralData.class);
+//		System.out.println("new GeneralData.id:" + bean.getId());	}
+	}
+
+}
