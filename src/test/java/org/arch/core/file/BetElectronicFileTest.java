0cb1623d5ef67928d5016cdfe344902c9139baf1
racsor <racsor@gmail.com>
2015-05-11 22:46:36 +0200

    ARC sysout

--- src/test/java/org/arch/core/file/BetElectronicFileTest.java
+++ src/test/java/org/arch/core/file/BetElectronicFileTest.java
@@ -21,6 +21,45 @@ public class BetElectronicFileTest {
 
 		String rdo[]; 
 		CalculateRigths aux = new CalculateRigths();
+		rdo = aux.Despliegue("7777111111111100", "NNNNNNNNNNNNNNNN", 1);
+
+		cb.setFechaJornada("010115");
+		cb.setNumTotalApuestas(StringUtils.leftPad(""+rdo.length, 6, '0'));
+		cb.setNumTotalBloques(StringUtils.leftPad(""+rdo.length, 6, '0'));
+		BetElectronicFile befile=new BetElectronicFile();
+		befile.setCabecera(cb);
+
+		RegistroBetElectronicFile[] registro=new RegistroBetElectronicFile[rdo.length];
+
+		System.out.println("numApuestas="+rdo.length);
+		for (int i = 0; i < rdo.length; i++) {
+			String linea = rdo[i];
+			RegistroBetElectronicFile registroBe=new RegistroBetElectronicFile();
+			registroBe.setNumApuestaBloque("1");
+			registroBe.setNumBloque(StringUtils.leftPad(""+i, 6, '0') );
+			registroBe.setPronostico15(StringUtils.right(linea, 2));
+			registroBe.setPronosticoPartido(StringUtils.right(linea, 14));
+			registro[i]=registroBe;
+			System.out.println(linea);
+		}
+		
+		befile.setRegistro(registro);
+		
+		
+		System.out.println(befile.getCabeceraString());
+		System.out.println(befile.getRegistroString());
+
+	}
+
+	
+	public void test10BetPleno15ElectronicFile() {
+		
+		HeaderBetElectronicFile cb = new HeaderBetElectronicFile();
+		cb.setIdDelegacion("64");
+		cb.setIdReceptor("65428");
+
+		String rdo[]; 
+		CalculateRigths aux = new CalculateRigths();
 		rdo = aux.Despliegue("77771111111111ff", "TTTTNNNNNNNNNNNN", 1);
 
 		cb.setFechaJornada("010115");
