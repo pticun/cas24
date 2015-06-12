0be4f38b8b4bf8ff312313e29f967a04f6c995f0
racsor <racsor@gmail.com>
2015-06-12 22:37:48 +0200

    resolver conflict

---
+++ src/main/java/org/alterq/domain/Company.java
@@ -0,0 +1,50 @@
+package org.alterq.domain;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Company implements Serializable {
+	@Id
+	private String id;
+	
+	private int type;
+	private boolean visibility;
+	private String nick;
+	private String description;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public boolean isVisibility() {
+		return visibility;
+	}
+	public void setVisibility(boolean visibility) {
+		this.visibility = visibility;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
--- src/main/java/org/alterq/domain/GeneralData.java
+++ src/main/java/org/alterq/domain/GeneralData.java
@@ -60,38 +60,6 @@ public class GeneralData implements Serializable {
 		this.id = id;
 	}
 
-	public int getType() {
-		return type;
-	}
-
-	public void setType(int type) {
-		this.type = type;
-	}
-
-	public boolean isVisibility() {
-		return visibility;
-	}
-
-	public void setVisibility(boolean visibility) {
-		this.visibility = visibility;
-	}
-
-	public String getNick() {
-		return nick;
-	}
-
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public int getNumBets() {
 		return numBets;
 	}
