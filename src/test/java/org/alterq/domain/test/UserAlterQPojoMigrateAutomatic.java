9d1c1a658fbf290ec58c6360eb10af2f59abdb37
racsor <racsor@gmail.com>
2015-10-14 21:34:22 +0200

    ARC migrate POJO not working

---
+++ src/test/java/org/alterq/domain/test/UserAlterQPojoMigrateAutomatic.java
@@ -0,0 +1,17 @@
+package org.alterq.domain.test;
+
+import org.alterq.domain.UserAlterQ;
+
+public class UserAlterQPojoMigrateAutomatic extends UserAlterQ {
+
+	int automatics;
+
+	public int getAutomatics() {
+		return automatics;
+	}
+
+	public void setAutomatics(int automatics) {
+		this.automatics = automatics;
+	}
+	
+}
