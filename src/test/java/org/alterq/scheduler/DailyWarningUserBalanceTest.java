7d879fbde638ddd84754c4ecaaa298e7715d58f8
racsor <racsor@gmail.com>
2015-11-04 21:55:07 +0100

    MVC debug bean declaration

--- src/test/java/org/alterq/scheduler/DailyWarningUserBalanceTest.java
+++ src/test/java/org/alterq/scheduler/DailyWarningUserBalanceTest.java
@@ -1,11 +1,18 @@
 package org.alterq.scheduler;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class DailyWarningUserBalanceTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/spring/applicationContext.xml" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)public class DailyWarningUserBalanceTest {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
